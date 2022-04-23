a61614655abec8a128e9771b315410d263c1f595
Mike <63655075+MixelTe@users.noreply.github.com>
2022-04-23 22:39:55 +0300

    fix

--- src/logger.py
+++ src/logger.py
@@ -35,20 +35,20 @@ def setLogging():
     log_formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(name)s     %(message)s')
     log_formatter2 = logging.Formatter('%(asctime)s %(levelname)-8s     %(message)s')
 
-    file_handler_error = logging.FileHandler("RecipeBook-errors.log", mode='w')
+    file_handler_error = logging.FileHandler("RecipeBook-errors.log", mode='a')
     file_handler_error.setFormatter(log_formatter)
     file_handler_error.setLevel(logging.WARNING)
     file_handler_error.encoding = "utf-8"
     logging.getLogger().addHandler(file_handler_error)
 
-    file_handler_info = logging.FileHandler("RecipeBook-info.log", mode='w')
+    file_handler_info = logging.FileHandler("RecipeBook-info.log", mode='a')
     file_handler_info.setFormatter(log_formatter2)
     file_handler_info.addFilter(InfoFilter())
     file_handler_info.encoding = "utf-8"
     logging.getLogger().addHandler(file_handler_info)
 
     otherLogger = logging.getLogger("other")
-    file_handler_other = logging.FileHandler("RecipeBook-other.log", mode='w')
+    file_handler_other = logging.FileHandler("RecipeBook-other.log", mode='a')
     file_handler_other.setFormatter(log_formatter2)
     file_handler_other.addFilter(OtherFilter())
     file_handler_other.encoding = "utf-8"
